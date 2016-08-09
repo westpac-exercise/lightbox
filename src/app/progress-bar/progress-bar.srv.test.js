8a79072220b6e42b8a8810cc86c1bb6a95bfb1a7
westpac-exercise <preethismeetme@gmail.com>
2016-08-10 09:12:25 +1000

    Update progress-bar.srv.test.js

--- src/app/progress-bar/progress-bar.srv.test.js
+++ src/app/progress-bar/progress-bar.srv.test.js
@@ -1,48 +1,3 @@
-/*describe('progressService test', function(){
-    describe('when I call progressService', function(){
-		
-		var service,http;
-		var mockconfig = { start: 1, finish:100, duration : 100 };
-		
-		// Arrange
-        beforeEach(module('lb-pb-app'));
-        beforeEach(inject(function(progressService) {
-    	service = progressService;
-    	}));
-
-		//Assert         
-        expect(service).not.toEqual(null);
-	
-        it('should fetch progress percentage on every heartbeat interval', inject(function(service,$timeout) {
-
-	    	var testProgress  = function(progress) {
-
-	    	var progressValue =progress.value;	
-	
-	    	expect(progressValue >= mockconfig.start).toBeTruthy();
-          	expect(progressValue <= mockconfig.finish).toBeTruthy();
-            };
-
-	    	var failTest = function(error) {
-     		 expect(error).toBeUndefined();
-   			 };
-
-	        //act 
-	        service.start(mockconfig)
-	        .then(null,null,testProgres)
-	     //   .catch(function(err){
-	     //   	done(new Error('Promise fail: ' + err));
-	     //   });
-	     $timeout.flush();        
-
-
-	    }));
-
-    });    
-
-});
-*/ 
-
 
 describe("Asynchronous specs", function() {
 var service,http;
@@ -83,23 +38,4 @@ service = "progressService";
    	 
   	});
 
- /* describe("long asynchronous specs", function() {
-    var originalTimeout;
-    beforeEach(function() {
-      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    });
-
-    it("takes a long time", function(done) {
-      setTimeout(function() {
-        done();
-      }, 9000);
-    });
-
-    afterEach(function() {
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-    });
-  });
-});
-
-*/
+ 
